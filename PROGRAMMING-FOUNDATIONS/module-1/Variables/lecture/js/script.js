2d52e17e0b13e8f92a587a6426de9caa0daf2963
Ludvig Alvir <ludvig.alvir@gmail.com>
2023-01-04 14:06:10 +0100

    added solutions

---
+++ PROGRAMMING-FOUNDATIONS/module-1/Variables/lecture/js/script.js
@@ -0,0 +1,11 @@
+let body = document.querySelector("body");
+
+let firstName = "Ola";
+
+body.innerHTML = "<h1>Velkommen " + firstName + "</h1>";
+body.innerHTML +=
+	"<h2 class ='heading'> Dette er tekst som kommer fra kode </h2>";
+
+body.style.background = "#333";
+
+firstName.length;
